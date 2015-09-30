3f2c0644b4741e72a40402eb39e20ae8935e7689
majj <603301704@qq.com>
2015-09-30 11:07:49 +0800

    链接handlebars模板引擎 添加

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+var port  = process.env.post || 3001;
+
+var app = express();
+
+app.engine('handlebars',handlebars.engine);
+// app.set('views','./views');
+app.set("view engine",'handlebars');
+app.set("port",port);
+
+app.get('/',function(req,res){
+	res.render('page1');
+	// res.type('text/html');
+	// res.status(400)
+	// res.send('调试成功');
+})
+
+app.listen(3001);
+
+console.log("已经打开端口为"+port);
